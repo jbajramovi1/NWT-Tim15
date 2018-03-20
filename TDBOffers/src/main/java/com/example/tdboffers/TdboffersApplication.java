73f0c1d56b736d2ebce4a89d3507996a72643794
Jasmina Bajramović <bajramovicjasmina2@gmail.com>
2018-03-20 23:40:36 +0100

    develop-Initiral project commit

---
+++ TDBOffers/src/main/java/com/example/tdboffers/TdboffersApplication.java
@@ -0,0 +1,12 @@
+package com.example.tdboffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdboffersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdboffersApplication.class, args);
+	}
+}
