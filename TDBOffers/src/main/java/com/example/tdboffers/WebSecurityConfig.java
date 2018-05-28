d4ee9181e8c8c2f58c9706900324b935c893b617
Ajša Terko <aterko1@etf.unsa.ba>
2018-05-28 14:56:11 +0200

    JWT Validation update

---
+++ TDBOffers/src/main/java/com/example/tdboffers/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.tdboffers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.tdboffers.security.JWTAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.GET,"/offer/getAll").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+}
