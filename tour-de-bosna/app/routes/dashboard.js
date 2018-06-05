5451c20b31c3d0821cb733568b90a540f6102cfb
Jasmina Bajramovic <jbajramovi1@etf.unsa.ba>
2018-06-05 23:27:41 +0200

    frontend - navbar

--- tour-de-bosna/app/routes/dashboard.js
+++ tour-de-bosna/app/routes/dashboard.js
@@ -1,5 +1,11 @@
 import Route from '@ember/routing/route';
-
+import { isEmpty } from '@ember/utils';
 export default Route.extend({
-  
+  authentication:Ember.inject.service(),
+  model: function(params, transition) {
+    return Ember.RSVP.hash({
+      role: this.controllerFor('welcome').get('userRole'),
+      isNotLoggedIn:isEmpty(null)
+      })
+  }
 });
